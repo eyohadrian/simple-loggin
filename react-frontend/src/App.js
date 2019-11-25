1db6afaef134beabf86743beef3db3060f1ec6ea
Adrian <adrian.asensio.bdn@gmail.com>
2019-11-25 08:55:32 +0100

    Loggin in progress

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,47 +1,69 @@
-import React, {useCallback, useRef, useState} from 'react';
-import {TextField} from "@material-ui/core";
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {TextField, withStyles} from "@material-ui/core";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Button from "@material-ui/core/Button";
 
-function App() {
-  const textValue = useRef("");
-  const urlValue = useRef();
-  const [responses, setResponses] = useState([]);
-  const [url, setUrl] = useState("http://127.0.0.1:8080/");
-  const send = useCallback(() => {
-    fetch(`${url}?value=${textValue.current}`, {
-      mode: 'cors',
-      headers: {
-        'Access-Control-Allow-Origin':'*',
-        'Access-Control-Allow-Credentials': 'true'
-      }
-    }).then(res => {
-      if(res.ok) {
-        return res.text();
-      }
-    }).then(res => setResponses(responses => [...responses, res]))
-      .catch(err => {
-      console.log(err)
-    });
-  }, [url]);
+const login = {
+  logged: "LOGGED",
+  rejected: "REJECTED",
+  notLogged: "NOT_LOGGED"
+};
 
-  const saveUrl = useCallback(() => setUrl(urlValue.current), []);
+const style = {
+  login: {
+    root: {
+
+    }
+  }
+};
+const Login = withStyles(style.login)(() => {
+
+  const fieldValues = useRef({
+    user: "",
+    password: ""
+  });
+
+  const login = useCallback(({onLogin}) => {
+    fetch("http://127.0.0.1:8080/login", {
+      method: "POST",
+      body: JSON.stringify(fieldValues.current),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }})
+      .then(res => {
+        if(res.status === 200) {
+          onLogin(login.logged)
+        } else {
+          setState(login.rejected)
+          onLogin(login.rejected)
+        }
+      })
+  }, []);
+
+  const onChangeField = useCallback((obj, param) => e => {
+    obj[param] = e.target.value;
+  }, []);
+
+  return (
+    <div>
+      <TextField label={"User"} onChange={onChangeField(fieldValues.current, "user")} />
+      <TextField label={"Password"} type={"password"} onChange={onChangeField(fieldValues.current, "password")} />
+      <Button onClick={login}>login</Button>
+    </div>
+  )
+});
+
+function App() {
+  const [state, setState] = useState(login.notLogged);
+  const onLogin = useCallback(state => {
+    setState(state)
+  }, []);
+
   return (
     <div className="App">
-      <div>
-        <h1>ULA ULA</h1>
-        <TextField defaultValue={url} onChange={e => urlValue.current = e.target.value}/>
-        <Button onClick={saveUrl}>Send</Button>
-      </div>
-      <div>
-        <h1>Send</h1>
-        <TextField onChange={e => textValue.current = e.target.value}/>
-        <Button onClick={send}>Send</Button>
-      </div>
-      <List>
-        {responses.map((value, index) => <ListItem key={`value-item-n-${index}`}>{value}</ListItem>)}
-      </List>
+      {state === login.logged ? <div>LOGGED</div> : <Login onLogin={onLogin}/>}
     </div>
   );
 }
